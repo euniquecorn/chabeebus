63359bd16dff2f9de9c5b0264852dc91e92c5ec8
Eunice Lois <behan@Eunices-MacBook-Air.local>
2023-05-15 07:52:15 +0800

    Reservation_Form-SeatNumber

--- app/controllers/Reservation_Form.php
+++ app/controllers/Reservation_Form.php
@@ -20,23 +20,42 @@ Class Reservation_Form extends Controller {
 
         $urlArray = splitURL();
         $busNo = $urlArray[2];
-        $result = [];
+        $schedule = [];
+        $seats = [];
         if (isset($busNo)) {
-            $sql = 'SELECT bus.bus_number, bus.location, schedules.dept_time, schedules.arrival_time
-            FROM schedules
-            JOIN bus ON schedules.bus_number = bus.bus_number
-            WHERE bus.bus_number = '. $busNo .'';
-
             $db = new Database();
-            $result = $db->read($sql);
+            $sql = 'SELECT bus.bus_number, bus.location, schedules.dept_time, schedules.arrival_time
+                FROM schedules
+                JOIN bus ON schedules.bus_number = bus.bus_number
+                WHERE bus.bus_number = '. $busNo .'';
+            $schedule = $db->read($sql);
+
+            $sql = 'SELECT * FROM reservations
+                JOIN bus ON reservations.bus_number = bus.bus_number
+                WHERE bus.bus_number = '. $busNo .'';
+            $seats = $db->read($sql);
+            $availableSeats = [];
+            $occupied = [];
+            foreach ($seats as $idx => $seat) {
+                $occupied[$idx + 1] = $seat->seat_number;
+            }
+
+            for ($i=1; $i <= $seats[0]->capacity; $i++) {
+                $found = array_search($i, $occupied);
+                if (!$found) {
+                    array_push($availableSeats, $i);
+                }
+            }
+            // show($availableSeats);
         } else {
             header("Location: {$_SERVER["HTTP_REFERER"]}");
         }
         
         $data['page_title'] = "BookReservation_Form";
         $data['title'] = "Reservation_Form";
-        $data['schedule'] = $result[0];
+        $data['schedule'] = $schedule[0];
         $data['date'] = date($date);
+        $data['availableSeats'] = $availableSeats;
         $this->view($data);
 
         $footer['title'] = 'common/footer';
