b5714a9984252405fa64f77f47caef9a080a0cb6
euniquecorn <beunicelois@gmail.com>
2023-05-15 09:22:07 +0800

    dashboard updates

--- app/controllers/admin.php
+++ app/controllers/admin.php
@@ -4,6 +4,25 @@ Class Admin extends Controller {
         $header['title'] = 'common/header-admin';
         $this->view($header);
         
+        $db = new Database();
+        $data['busSales'] = $db->read("SELECT SUM(r.price) as total_sales, b.bus_number as bus_number
+            FROM buses b
+            LEFT JOIN reservations r on r.bus_number = b.bus_number
+            GROUP BY b.bus_number
+            ORDER BY b.bus_number
+        ");
+
+        $data['reservations'] = $db->read("SELECT * FROM reservations r
+            ORDER BY r.dates DESC
+        ");
+        $data['sales'] = $db->read("SELECT SUM(reservations.price) as total_sales FROM reservations")[0];
+        $data['salesToDate'] = $db->read("SELECT SUM(r.price) as total_sales, r.dates
+            FROM reservations r
+            GROUP BY r.dates
+            ORDER BY r.dates DESC
+
+        ");
+
         $data['page_title'] = "Admin";
         $data['title'] = 'admin';
         $this->view($data);
